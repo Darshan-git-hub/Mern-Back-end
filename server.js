52f1d333c383e035a6f952caf47772482aa1891a
Krisanth-21 <darshanu@karunya.edu.in>
2025-04-22 20:58:59 +0530

    suiiii

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const path = require('path');
 const User = require('./models/User');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 // Middleware
 const allowedOrigins = [
